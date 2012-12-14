71580037eca5e091968a2e98762bc4ddb228d529
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2012-12-14 13:40:06 +0100

    Adding first yeoman init skeleton

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gumpApp = angular.module('gumpApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
