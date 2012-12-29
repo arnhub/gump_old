1f314b8e91a6fe72bc83758ef03f822e586933f9
ArnaudSpanneut <arnaud.spanneut@gmail.com>
2012-12-29 22:30:10 +0100

    Adding socket.io client for dashboard

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,14 +1,11 @@
 'use strict';
 
-gumpApp.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-  $http({method: 'GET', url: '/api/game'}).
-      success(function (data, status, headers, config) {
-        $scope.game = data;
-      }).
-      error(function (data, status, headers, config) {
-        console.log(data, status, headers, config);
-        // called asynchronously if an error occurs
-        // or server returns response with status
-        // code outside of the <200, 400) range
-      });
+gumpApp.controller('MainCtrl', ['$scope', function ($scope) {
+    var dashboard = io.connect('/dashboard');
+
+    dashboard.on('game-updated', function (game) {
+        $scope.$apply(function () {
+          $scope.game = game;
+        });
+    });
 }]);
