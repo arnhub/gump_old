8a39f36bfbc9a68ae0fdc23bdc08c04ba61b75be
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2012-12-29 18:42:56 +0100

    Using server to retrieve game and its players

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,19 +1,14 @@
 'use strict';
 
-gumpApp.controller('MainCtrl', ['$scope', function ($scope) {
-  var player1 = {
-    firstName: 'Hubert',
-    lastName: 'SABLONNIERE',
-    email: 'hubert.sablonniere@gmail.com',
-    score: 7
-  };
-  var player2 = {
-    firstName: 'Arnaud',
-    lastName: 'SPANNEUT',
-    email: 'arnaud.spanneut@gmail.com',
-    score: 11
-  };
-
-  $scope.player1 = player1;
-  $scope.player2 = player2;
+gumpApp.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http({method: 'GET', url: '/api/game'}).
+      success(function (data, status, headers, config) {
+        $scope.game = data;
+      }).
+      error(function (data, status, headers, config) {
+        console.log(data, status, headers, config);
+        // called asynchronously if an error occurs
+        // or server returns response with status
+        // code outside of the <200, 400) range
+      });
 }]);
